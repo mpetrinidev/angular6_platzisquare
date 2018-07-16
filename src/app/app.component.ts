77f07ee20811763b3361d8ce951b07b4dc2c0b9a
mpetrini <dev.mpetrini@gmail.com>
2018-07-16 00:11:58 -0300

    binding en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'PlatziSquare';
+  a = 2;
+  b = 3;
+  listo = false;
+  nombre = 'Mauro';
+
+  constructor() {
+    setTimeout(() => {
+      this.listo = true;
+    }, 3000);
+  }
+
+  hacerAlgo() {
+    alert('Haciendo algo');
+  }
 }
