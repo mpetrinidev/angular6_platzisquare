c7d3c3fa6f13a82874764e667e486b061b41c382
mpetrini <dev.mpetrini@gmail.com>
2018-07-28 21:57:45 -0300

    finalizando las animaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from 'angularfire2';
@@ -41,6 +42,7 @@ export const firebaseConfig = {
   imports: [
     Routing,
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     AngularFireModule.initializeApp(firebaseConfig),
--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { LugaresService } from '../services/lugares.service';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/animations';
 
 @Component({
   selector: 'app-lugares',
   templateUrl: './lugares.component.html',
-  styleUrls: ['./lugares.component.css']
+  styleUrls: ['./lugares.component.css'],
+  animations: [
+    // trigger('contenedorAnimable', [
+    //   state(
+    //     'inicial',
+    //     style({
+    //       opacity: 0,
+    //       backgroundColor: 'green',
+    //       transform: 'rotate3d(0,0,0,0deg)'
+    //     })
+    //   ),
+    //   state(
+    //     'final',
+    //     style({
+    //       opacity: 1,
+    //       backgroundColor: 'yellow',
+    //       transform: 'rotate3d(5,10,20,30deg)'
+    //     })
+    //   ),
+    //   transition('inicial => final', animate(1000)),
+    //   transition('final => inicial', animate(500))
+    // ]),
+    trigger('contenedorAnimable', [
+      state(
+        'inicial',
+        style({
+          opacity: 0
+        })
+      ),
+      state(
+        'final',
+        style({
+          opacity: 1
+        })
+      ),
+      transition('inicial => final', animate(2000)),
+      transition('final => inicial', animate(1000))
+    ])
+  ]
 })
 export class LugaresComponent implements OnInit {
   lat: number = -34.6351364;
   lng: number = -58.4852413;
   lugares: any;
+  state = 'inicial';
 
   constructor(private lugaresService: LugaresService) {}
 
@@ -20,10 +66,19 @@ export class LugaresComponent implements OnInit {
       .subscribe(
         res => {
           this.lugares = Object.values(res);
+          this.state = 'final';
         },
         error => {
           alert(error.statusText);
         }
       );
   }
+
+  animar() {
+    this.state = this.state === 'final' ? 'inicial' : 'final';
+  }
+
+  animacionInicial(e) {}
+
+  animacionTermina(e) {}
 }
